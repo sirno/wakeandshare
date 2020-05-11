64aa154c90dbf84cc1c4d21ae07defde4bdf0fa4
Nicolas Ochsner <ochsnern@student.ethz.ch>
2020-05-11 12:51:25 +0200

    Add Checkmark.

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,9 +1,7 @@
 <template>
-  <div
-    class="checkbox"
-    v-bind:class="{ checked: checked }"
-    v-on:click="toggle()"
-  ></div>
+  <div class="checkbox" v-bind:class="{ checked: checked }" v-on:click="toggle()">
+    <span v-if="checked">X</span>
+  </div>
 </template>
 
 <script>
@@ -12,15 +10,15 @@ export default {
   props: {
     checked: {
       type: Boolean,
-      default: true,
-    },
+      default: true
+    }
   },
   methods: {
     toggle: function() {
       this.isChecked = !this.isChecked;
       this.$emit("toggled");
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -32,7 +30,11 @@ $size: 25px
 $border-size: 0.2rem
 
 .checkbox
-  display: inline-block
+  display: inline-flex
+  position: relative
+  flex-direction: row
+  align-items: center
+  justify-content: space-around
   margin-right: 2rem
   margin-left: 2rem
   width: $size
@@ -41,9 +43,11 @@ $border-size: 0.2rem
   border-style: solid
   border-color: $pink
   +shadow($border-size, $pink)
-
 .checked
   background-color: $yellow
   border-color: $blue
   +shadow($border-size, $blue)
+.checkbox span
+  position: absolute
+  font-size: 1.5rem
 </style>
